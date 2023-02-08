1f3d4a34e7aadaa41aa46a991fdaed3c9165436a
Tom Boettger <t.boettger@live.de>
2023-02-08 20:11:34 +0100

    Indent debug messages depending on nodeID

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -3,12 +3,24 @@ package utils
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func Logf(format string, args ...interface{}) {
-	currentTime := time.Now().Format("2006-01-02 15:04:05.000")
-	fmt.Printf(currentTime+" ["+os.Getenv("NODE_ID")+"] "+format+"\n", args...)
+	if os.Getenv("DEBUG") == "true" {
+		currentTime := time.Now().Format("2006-01-02 15:04:05.000")
+		nodeId, err := strconv.Atoi(os.Getenv("NODE_ID"))
+		if err != nil {
+			panic(err)
+		}
+		indent := ""
+		if nodeId > 1 {
+			indent = strings.Repeat(" ", (nodeId-1)*4)
+		}
+		fmt.Printf(currentTime+indent+" ["+os.Getenv("NODE_ID")+"] "+format+"\n", args...)
+	}
 }
 
 func IntMin(a, b int) int {
