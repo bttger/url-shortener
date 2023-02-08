ead33b00e7f4715b30b09e43357ba33a45e714d6
Tom Boettger <t.boettger@live.de>
2023-02-08 22:13:26 +0100

    Add debug env var to startup script

--- startRaftCluster.sh
+++ startRaftCluster.sh
@@ -4,7 +4,7 @@ CLUSTER_SIZE=5
 USE_PORTS_FROM=9000
 
 for (( i=1; i<=CLUSTER_SIZE; i++ ));do
-    USE_PORTS_FROM=$USE_PORTS_FROM NODE_ID=$i CLUSTER_SIZE=$CLUSTER_SIZE go run ./cmd/joinNode/main.go &
+    DEBUG="true" USE_PORTS_FROM=$USE_PORTS_FROM NODE_ID=$i CLUSTER_SIZE=$CLUSTER_SIZE go run ./cmd/joinNode/main.go NODE_ID=$i &
 done
 
 wait
